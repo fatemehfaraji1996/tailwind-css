f979d8475c1d42c0fc7b7a67c3fa380b38f1fc28
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-08-03 18:58:19 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+function App() {
+  return (
+  <>
+      <p className="text-5xl">Vite + React</p>
+      <h1 className="italic">po</h1>
+    
+  </>
+  );
+}
+
+export default App;
